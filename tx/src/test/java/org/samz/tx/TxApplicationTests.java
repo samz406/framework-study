c5e1c409e6a8472c1086c3b58368b1200a0929cf
lilin <samz406@foxmail.com>
2022-03-21 19:43:55 +0800

    add seata

---
+++ tx/src/test/java/org/samz/tx/TxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samz.tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
