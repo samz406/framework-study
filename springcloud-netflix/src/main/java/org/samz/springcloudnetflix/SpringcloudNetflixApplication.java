9ee2a37371c7c40ce2df62e77b93fbcb1e2fe233
lilin <samz406@foxmail.com>
2022-03-21 19:26:02 +0800

    add fuction module

---
+++ springcloud-netflix/src/main/java/org/samz/springcloudnetflix/SpringcloudNetflixApplication.java
@@ -0,0 +1,13 @@
+package org.samz.springcloudnetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudNetflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNetflixApplication.class, args);
+    }
+
+}
---
+++ springcloud-netflix/src/test/java/org/samz/springcloudnetflix/SpringcloudNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samz.springcloudnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudNetflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
