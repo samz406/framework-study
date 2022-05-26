68855c009fe468a2e3e865fea148a7e570ff02b0
lilin <samz406@foxmail.com>
2022-05-26 12:37:18 +0800

    add order mq

---
+++ mq/src/main/java/org/samz/rocketmq/OrderMessage.java
@@ -0,0 +1,54 @@
+package org.samz.rocketmq;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.List;
+
+/**
+ * @author: lilin
+ * @Date: 2022/5/26 10:58
+ * @Description: 顺序消息
+ */
+public class OrderMessage {
+
+
+    public static void main(String[] args) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+
+        TransactionMQProducer producer = new TransactionMQProducer("testOrderMsgGroup");
+
+        producer.setNamesrvAddr("192.192.192.61:9876");
+        producer.start();
+        MsgSelector msgSelector = new MsgSelector();
+
+        for (int i = 0; i < 100; i++) {
+            int orderId = i % 10;
+            String msg = "Hello RocketMQ" + i;
+            Message message = new Message("testOrderMsg", msg.getBytes());
+            final SendResult send = producer.send(message, msgSelector, orderId);
+
+            System.out.println(send);
+        }
+
+    }
+
+    /**
+     * 发送队列选择器
+     */
+    static class MsgSelector implements MessageQueueSelector {
+
+        @Override
+        public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+
+            Integer id = (Integer) arg;
+            int index = id % mqs.size();
+            return mqs.get(index);
+        }
+    }
+}
---
+++ mq/src/main/java/org/samz/rocketmq/OrderMsgConsumer.java
@@ -0,0 +1,44 @@
+package org.samz.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author: lilin
+ * @Date: 2022/5/26 11:28
+ * @Description:
+ */
+public class OrderMsgConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer("ProducerGroup");
+
+        defaultMQPushConsumer.setNamesrvAddr("192.192.192.61:9876");
+        defaultMQPushConsumer.subscribe("testOrderMsg", "*");
+        defaultMQPushConsumer.setMessageModel(MessageModel.CLUSTERING);
+
+        //  defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        defaultMQPushConsumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+
+                System.out.printf("%s 获取的消息 %s \n", LocalDateTime.now(), msgs);
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        defaultMQPushConsumer.start();
+
+
+    }
+}
